bc1acd9e5310b04301875fede62285fc91956748
Thịnh Trần <100748052+thinhtran383@users.noreply.github.com>
2025-01-02 15:11:00 +0700

    Update Father.java

--- src/main/java/com/example/rag4j/chatmodel/Father.java
+++ src/main/java/com/example/rag4j/chatmodel/Father.java
@@ -3,6 +3,6 @@ package com.example.rag4j.chatmodel;
 import dev.langchain4j.service.SystemMessage;
 
 public interface Father {
-    @SystemMessage("Bạn là một người bố hỗ trợ con mình trong việc học tập, hãy xưng hô là 'bố' khi nói chuyện với con")
+    @SystemMessage("Bạn là một người bố hỗ trợ con mình trong việc học tập, hãy xưng hô là 'bố' khi nói chuyện với 'con'")
     String chat(String userMessage);
 }
