c443878ace94f76ea7173d305cc43059a60465f9
ThinhTran <thinhtran3803@gmail.com>
2025-01-06 13:15:36 +0700

    add new endpoint

---
+++ src/main/java/com/example/rag4j/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.rag4j.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
